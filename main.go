1be29df48283f7df1dd5b75f8853d91db2a4f664
Anant-raj2 <heyanantraj@gmail.com>
2024-12-08 20:54:59 -0600

    tryung3

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	_ "github.com/tursodatabase/libsql-client-go/libsql"
 	"tutorme.com/api"
 	"tutorme.com/internal/repository"
@@ -17,11 +16,6 @@ import (
 )
 
 func run() error {
-	err := godotenv.Load()
-	if err != nil {
-		return err
-	}
-
 	url, err := util.ResolveEnv("TURSO_DATABASE_URL")
 	if err != nil {
 		return err
