2bc2beca19d261c1e00c4a2834da47715efee70f
Anant-raj2 <heyanantraj@gmail.com>
2024-12-08 23:03:29 -0600

    finished

--- api/handler.go
+++ api/handler.go
@@ -1,19 +1,13 @@
 package api
 
 import (
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"tutorme.com/internal/repository"
 	"tutorme.com/types"
-	"tutorme.com/util"
 )
 
 func (s *Server) CreateOrganization(c *fiber.Ctx) error {
@@ -53,47 +47,3 @@ func (s *Server) CreateOrganization(c *fiber.Ctx) error {
 	return c.JSON(finalResp)
 }
 
-func IssueTursoDatabase(dbName string) (types.TursoDatabase, error) {
-	body := []byte(fmt.Sprintf(`{"name": "%s", "group": "default"}`, dbName))
-	fmt.Println(string(body))
-
-	reqBody := bytes.NewReader(body)
-
-	client := &http.Client{
-		Timeout: time.Second * 6,
-	}
-
-	requestUrl := fmt.Sprintf("https://api.turso.tech/v1/organizations/%s/databases", os.Getenv("TURSO_ORGANIZATION_SLUG"))
-	tursoToken, err := util.ResolveEnv("TURSO_API_TOKEN")
-	if err != nil {
-		return types.TursoDatabase{}, err
-	}
-	bearer := "Bearer " + tursoToken
-
-	req, err := http.NewRequest(http.MethodPost, requestUrl, reqBody)
-
-	req.Header.Set("Authorization", bearer)
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return types.TursoDatabase{}, err
-	}
-
-	jsonResp := make([]byte, 1024)
-	n, err := resp.Body.Read(jsonResp)
-
-	if err != nil {
-		if err != io.EOF {
-			return types.TursoDatabase{}, err
-		}
-	}
-
-	var tursoDbResp types.TursoDatabase
-
-	if err := json.Unmarshal(jsonResp[:n], &tursoDbResp); err != nil {
-		return types.TursoDatabase{}, err
-	}
-
-	return tursoDbResp, nil
-}
--- api/service.go
+++ api/service.go
@@ -1,10 +1,18 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
 	"log/slog"
+	"net/http"
+	"os"
+	"time"
 
 	"github.com/cloudflare/cloudflare-go"
+	"tutorme.com/types"
 	"tutorme.com/util"
 )
 
@@ -51,3 +59,47 @@ func CreateDNSRecord(ctx context.Context, subdomain string) error {
 	return nil
 }
 
+func IssueTursoDatabase(dbName string) (types.TursoDatabase, error) {
+	body := []byte(fmt.Sprintf(`{"name": "%s", "group": "default"}`, dbName))
+	fmt.Println(string(body))
+
+	reqBody := bytes.NewReader(body)
+
+	client := &http.Client{
+		Timeout: time.Second * 6,
+	}
+
+	requestUrl := fmt.Sprintf("https://api.turso.tech/v1/organizations/%s/databases", os.Getenv("TURSO_ORGANIZATION_SLUG"))
+	tursoToken, err := util.ResolveEnv("TURSO_API_TOKEN")
+	if err != nil {
+		return types.TursoDatabase{}, err
+	}
+	bearer := "Bearer " + tursoToken
+
+	req, err := http.NewRequest(http.MethodPost, requestUrl, reqBody)
+
+	req.Header.Set("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return types.TursoDatabase{}, err
+	}
+
+	jsonResp := make([]byte, 1024)
+	n, err := resp.Body.Read(jsonResp)
+
+	if err != nil {
+		if err != io.EOF {
+			return types.TursoDatabase{}, err
+		}
+	}
+
+	var tursoDbResp types.TursoDatabase
+
+	if err := json.Unmarshal(jsonResp[:n], &tursoDbResp); err != nil {
+		return types.TursoDatabase{}, err
+	}
+
+	return tursoDbResp, nil
+}
